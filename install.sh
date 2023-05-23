a134e452e0a7915099127628f935e7024ec2611d
NullNUMMER24 <j.rohrbach@sensemail.ch>
2023-05-23 22:19:16 +0200

    Alacritty theme added

--- install.sh
+++ install.sh
@@ -1,8 +1,30 @@
 yay -S neovim 
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 yay -S obsidian
 yay -S onedriver
-yay -S steam
+##########################################################################
+# Spicetify
+##########################################################################
 yay -S spotify
-yay -S minecraft-launcher
+# Change the permissions of the spotify files
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
+# Download themes from: https://github.com/spicetify/spicetify-themes
+yay -S spicetify-themes-git
+##########################################################################
+# Terminal
+##########################################################################
 yay -S alacritty-git
+mkdir -p ~/.config/alacritty
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
+yay -S nitch
+cp ~/git/dotfiles/.bashrc ~/
+##########################################################################
+# Games
+##########################################################################
+yay -S minecraft-launcher
+yay -S steam
+
+
+
 yay -S hyprland
