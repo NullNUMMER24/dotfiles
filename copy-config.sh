dfef831e56eb436f4803b902fb3a831dc36c0757
NullNUMMER24 <j.rohrbach@sensemail.ch>
2023-06-03 22:46:25 +0200

    i3 error fixed

--- copy-config.sh
+++ copy-config.sh
@@ -1,5 +1,5 @@
-cp config ~/.config/i3/config
-cp picom.conf /etc/xdg/picom.conf
+cp ~/git/dotfiles/i3/ ~/.config/i3/
+cp ~/git/dotfiles/picom/ /etc/xdg/picom.conf
 mkdir -p ~/.config/polybar/
 cp ~/git/dotfiles/polybar/config.ini ~/.config/polybar/config
 cp ~/git/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
