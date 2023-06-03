746682f19ad4165b80da5a157e0728745fa13adf
NullNUMMER24 <j.rohrbach@sensemail.ch>
2023-06-03 22:06:58 +0200

    polybar added

--- copy-config.sh
+++ copy-config.sh
@@ -1,2 +1,5 @@
 cp config ~/.config/i3/config
 cp picom.conf /etc/xdg/picom.conf
+mkdir -p ~/.config/polybar/
+cp ~/git/dotfiles/polybar/config.ini ~/.config/polybar/config
+cp ~/git/dotfiles/polybar/launch.sh ~/.config/polybar/launch.sh
